d2f0e4926e584150ff9540b2bf705a77b51eb7c7
Seb <121025605+R4W4U@users.noreply.github.com>
2026-07-29 09:12:47 +0200

    Implement key presses for loop failure condition
    Added functionality to press KEYPAD_PLUS and F10 when loop result is false.

--- code.py
+++ code.py
@@ -4,6 +4,7 @@ import adafruit_ducky
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 
+from adafruit_hid.keycode import Keycode
 
 keyboard = Keyboard(usb_hid.devices)
 
@@ -20,4 +21,8 @@ time.sleep(1)
 while True:
     result = duck.loop()
     if result is False:
+        keyboard.press(Keycode.KEYPAD_PLUS)
+        keyboard.release_all()
+        keyboard.press(Keycode.F10)
+        keyboard.release_all()
         break
